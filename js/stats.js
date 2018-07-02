09a576e1ba8ebc4f8b1aed926757111477a6b1a7
cancabron <cancabron@gmail.com>
2018-07-02 17:39:19 +0200

    Changed Volume for MarketCap block

--- js/stats.js
+++ js/stats.js
@@ -16,6 +16,14 @@ $( "#volume" ).attr( "data-to", volume.volume );
 });
 
 $(function() {
+  $.getJSON('https://actinium.org:3000/v1/acm/marketcap', function(marketcap) {
+
+$( "#marketcap" ).attr( "data-to", marketcap.USD );
+
+})
+});
+
+$(function() {
 	$.getJSON('https://actinium.org:3000/v1/acm/supply', function(supply) {
 
 $( "#supply" ).attr( "data-to", supply.total );
