2577aa6beafbeba51ba88ec8434c59bc9b548ff3
cancabron <cancabron@gmail.com>
2018-07-03 20:14:17 +0200

    Working stats on mobile & desktop (OLÉ)

---
+++ js/statsV2.js
@@ -0,0 +1,125 @@
+$(function() {
+  $.getJSON('https://actinium.org:3000/v1/acm/price', function(price) {
+
+$( "#price" ).attr( "data-to", price.price*100000000 );
+$( "#volume" ).attr( "data-to", price.volume );
+
+$('#price').countTo({
+  from: 0,
+  to: price.price*100000000,
+  speed: 3000,
+  refreshInterval: 80,
+  formatter: function (value, options) {
+    return value.toFixed(options.decimals);
+  },
+  onUpdate: function (value) {
+    console.debug(this);
+  },
+  onComplete: function (value) {
+    console.debug(this);
+  }
+});
+
+})
+});
+
+$(function() {
+  $.getJSON('https://actinium.org:3000/v1/acm/fiatprice', function(fiatprice) {
+
+$( "#fiatprice" ).attr( "data-to", fiatprice.USD );
+
+$('#fiatprice').countTo({
+  from: 0,
+  to: fiatprice.USD ,
+  speed: 3000,
+  refreshInterval: 80,
+  decimals: 2,
+  formatter: function (value, options) {
+    return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+  },
+  onUpdate: function (value) {
+    console.debug(this);
+  },
+  onComplete: function (value) {
+    console.debug(this);
+  }
+});
+
+})
+});
+
+$(function() {
+  $.getJSON('https://actinium.org:3000/v1/acm/marketcap', function(marketcap) {
+
+$( "#marketcap" ).attr( "data-to", marketcap.USD );
+
+$('#marketcap').countTo({
+  from: 0,
+  to: marketcap.USD,
+  speed: 3000,
+  refreshInterval: 80,
+  decimals: 0,
+  formatter: function (value, options) {
+    return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+  },
+  onUpdate: function (value) {
+    console.debug(this);
+  },
+  onComplete: function (value) {
+    console.debug(this);
+  }
+});
+
+})
+});
+
+$(function() {
+  $.getJSON('https://actinium.org:3000/v1/acm/supply', function(supply) {
+
+$( "#supply" ).attr( "data-to", supply.total );
+
+$('#supply').countTo({
+  from: 0,
+  to: supply.total,
+  speed: 3000,
+  refreshInterval: 80,
+  decimals: 0,
+  formatter: function (value, options) {
+    return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+  },
+  onUpdate: function (value) {
+    console.debug(this);
+  },
+  onComplete: function (value) {
+    console.debug(this);
+  }
+});
+
+
+})
+});
+
+$(function() {
+$.getJSON('https://actinium.org:3000/v1/acm/hashrate', function(hashrate) {
+
+$( "#hashrate" ).attr( "data-to", hashrate.gh );
+
+$('#hashrate').countTo({
+  from: 0,
+  to: hashrate.gh,
+  speed: 3000,
+  refreshInterval: 80,
+  decimals: 0,
+  formatter: function (value, options) {
+    return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+  },
+  onUpdate: function (value) {
+    console.debug(this);
+  },
+  onComplete: function (value) {
+    console.debug(this);
+  }
+});
+
+})
+});
